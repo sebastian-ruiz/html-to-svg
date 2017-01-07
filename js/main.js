5e20cec9a267db4b85c967dfbbd04cb54f191974
Sebastian Ruiz <sebastian.ruiz@me.com>
2017-01-07 17:28:21 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function saveTextAsFile()
+{
+    var textToSave = document.getElementById("inputTextToSave").value;
+    var textToSaveAsBlob = new Blob([textToSave], {type:"text/plain"});
+    var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
+    var fileNameToSaveAs = "image.svg";
+ 
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    downloadLink.href = textToSaveAsURL;
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+ 
+    downloadLink.click();
+}
+ 
+function destroyClickedElement(event)
+{
+    document.body.removeChild(event.target);
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# HTML to SVG
+
+This is a very simple tool to save html as `image.svg`.
